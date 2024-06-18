f7bf315c99b23be0f4319d169fd91a089635bbbf
Alexey Yurk <alexeyyurk@gmail.com>
2024-06-19 00:14:02 +0500

    Домашние задания 4 - 6

---
+++ 5-languages/app.js
@@ -0,0 +1,15 @@
+const language = 'de';
+
+switch (language) {
+    case 'ru':
+        console.log('Добро пожаловать');
+        break;
+    case 'de':
+        console.log('Willkommen zurück');
+        break;
+    case 'en':
+        console.log('Welcome');
+        break;
+    default:
+        console.log('Здравствуйте! Ваш язык не определен.');
+}   
